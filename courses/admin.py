e48f5666e60371bc5f635acf416513da2160e896
Amanda <awake@bu.edu>
2018-11-01 14:30:51 -0400

    updated the admin so that the types of questions were linked to the quizzes and reordered the fields in the Quiz admin.

--- courses/admin.py
+++ courses/admin.py
@@ -3,9 +3,35 @@ from django.contrib import admin
 from . import models
 
 
+class TextInLine(admin.StackedInline):
+    model = models.Text
+
+
+class QuizInLine(admin.StackedInline):
+    model = models.Quiz
+
+
+class AnswerInLine(admin.StackedInline):
+    model = models.Answer
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TextInLine, QuizInLine]
+
+    search_fields = ['title', 'description']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInLine, ]
+
+
+class QuizAdmin(admin.ModelAdmin):
+    fields = ['course', 'title', 'description', 'order', 'total_questions']
+
+
 admin.site.register(models.Course)
 admin.site.register(models.Text)
-admin.site.register(models.Quiz)
-admin.site.register(models.MultipleChoiceQuestion)
-admin.site.register(models.TrueFalseQuestion)
+admin.site.register(models.Quiz, QuizAdmin)
+admin.site.register(models.MultipleChoiceQuestion, QuestionAdmin)
+admin.site.register(models.TrueFalseQuestion, QuestionAdmin)
 admin.site.register(models.Answer)
