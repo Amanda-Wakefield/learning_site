3de441958d4013ef37ccba557c3987c40a88778a
Amanda <awake@bu.edu>
2018-11-02 03:49:47 -0400

    added a custom filter to sort courses by year created

--- courses/admin.py
+++ courses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from datetime import date
 
 from . import models
 
@@ -15,12 +16,41 @@ class AnswerInLine(admin.StackedInline):
     model = models.Answer
 
 
+class YearListFilter(admin.SimpleListFilter):
+    title = 'year created'
+    parameter_name = 'year'  # Shows up in url
+
+    def lookups(self, request, model_admin):
+        return (
+            ('2018', '2018'),  # one will go in url and one will go in sidebar
+            ('2017', '2017'),
+            ('2016', '2016'),
+            ('2015', '2015'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '2018':
+            return( queryset.filter(created_at__gte=date(2018, 1, 1),
+                                   created_at__lte=date(2018, 12, 31)))
+
+        if self.value() == '2017':
+            return( queryset.filter(created_at__gte=date(2017, 1, 1),
+                                   created_at__lte=date(2017, 12, 31)))
+
+        if self.value() == '2016':
+            return( queryset.filter(created_at__gte=date(2016, 1, 1),
+                                   created_at__lte=date(2016, 12, 31)))
+
+        if self.value() == '2015':
+            return( queryset.filter(created_at__gte=date(2015, 1, 1),
+                                   created_at__lte=date(2015, 12, 31)))
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [TextInLine, QuizInLine]
 
     search_fields = ['title', 'description']
 
-    list_filter = ['created_at', 'published']
+    list_filter = ['created_at', 'published', YearListFilter]
 
 
 class QuestionAdmin(admin.ModelAdmin):
